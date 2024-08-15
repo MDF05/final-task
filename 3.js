4b9e27117904fdd793f1bce536874d98eb16a840
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-15 21:51:19 +0700

    fix: rm bootstrap

---
+++ 3.js
@@ -0,0 +1,29 @@
+function draw(num) {
+    let pola = ""
+    if(num % 2 == 0) return console.log("harus bilangan ganjil ya")
+
+    for(let a = 0; a < num; a++) {
+        for(let b = 0; b < num; b++) {
+            if(a == 0 || a == (num - 1)) {
+                if( b == 0) pola += "*"
+                else if( b == (num - 1) / 2) pola += "*"
+                else if( b == num - 1) pola += "*"
+                else pola += "#" 
+            }else if(a == (num -1) / 2) {
+                if(b == (num - 1) /2 ) pola += "#"
+                else pola += "*"
+            } 
+            else {
+               if(b == (num - 1)/2) pola += "*"
+                else pola += "#" 
+            }
+        }
+        pola += "\n"
+        
+    }
+
+    console.log(pola)
+}
+
+
+draw(11)
